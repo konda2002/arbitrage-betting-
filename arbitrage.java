e0509a5c398d0043a11f189f45f8c60c0c918ff5
konda2002 <73963822+konda2002@users.noreply.github.com>
2023-01-09 13:59:55 -0500

    Update arbitrage.java
    created the method and added the prob for draw

--- arbitrage.java
+++ arbitrage.java
@@ -1,52 +1,69 @@
 import java.util.ArrayList;
 
+
+
+
+
+
 public class Arbitrage {
+     
+     
+
+public static double calcprob(double odds){
+
+    if(odds>0){
+
+        Double temp=(100)/(100+odds);
+return temp;        
+        }else{
+            double temp=(200)/(100+ ((-1)*odds) );
+return temp;        
+            }
+
+
+
+}
+
+
+
+
      public static void main(String[] args) {
     
 
-//index 1 from every arraylist will be the data froom the same sportsbooking app
+//index 1 from every arraylist will be the data from the same sportsbooking app
 
     ArrayList<Double> oddsofteam1= new ArrayList<>();
     ArrayList<Double> oddsofteam2= new ArrayList<>();
+    ArrayList<Double> oddsofdraw= new ArrayList<>();
+
     ArrayList<Double> probofteam1= new ArrayList<>();
     ArrayList<Double> probofteam2= new ArrayList<>();
+    ArrayList<Double> probofdraw= new ArrayList<>();
 
-
-    for(int i=0;i<oddsofteam1.size();i++){
-
-        
-if(oddsofteam1.get(i)>0){
-
-Double temp=(100)/(100+oddsofteam1);
-    probofteam1.add(i, temp);
-
-}else if(oddsofteam1.get(i)<0){
-    double temp=(200)/(100+((-1)*oddsfrom1));
-    probofteam1.add(i, temp);
-
-    }
     
-    }
+
+for(int i=0;i<oddsofteam1.size();i++){
+
+    double temp=calcprob(oddsofteam1.get(i));
+probofteam1.set(i,temp);
+
+}
 
 
 
-    for(int i=0;i<oddsofteam2.size();i++){
+for(int i=0;i<oddsofteam2.size();i++){
 
-        
-        if(oddsofteam2.get(i)>0){
-        
-        Double temp=(100)/(100+oddsofteam2);
-            probofteam2.add(i, temp);
-        
-        }else if(oddsofteam2.get(i)<0){
-            double temp=(200)/(100+((-1)*oddsfrom1));
-            probofteam2.add(i, temp);
-        
-            }
-            
-            }
-        
-        
+    double temp=calcprob(oddsofteam2.get(i));
+probofteam2.set(i,temp);
+
+}        
+
+for(int i=0;i<oddsofdraw.size();i++){
+
+    double temp=calcprob(oddsofdraw.get(i));
+probofdraw.set(i,temp);
+
+}
 
 
 
