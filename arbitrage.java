14aef2eb87c966158b1af441ba7f7b3d52161602
konda2002 <73963822+konda2002@users.noreply.github.com>
2023-01-09 15:19:29 -0500

    Update arbitrage.java
    1.not sure about the last part have to still check it. it tries every possible combination with odds from different sports books and checks if there is an arbitrage opportunity. 
    
    2.Need to connect to the Api to get the live data into the array list

--- arbitrage.java
+++ arbitrage.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 
 
 public class Arbitrage {
-     
-     
 
 public static double calcprob(double odds){
 
@@ -56,13 +54,40 @@ for(int i=0;i<oddsofteam2.size();i++){
     double temp=calcprob(oddsofteam2.get(i));
 probofteam2.set(i,temp);
 
-}        
+}
+
 
 for(int i=0;i<oddsofdraw.size();i++){
 
     double temp=calcprob(oddsofdraw.get(i));
 probofdraw.set(i,temp);
 
+}
+
+// the values of these ints give the respective booky odds we found the arbitrage opportunity with, suppose 1 2 3, we found an opportunity using odds for team1 with booky 1, team2 with booky 2, draw with booky 3 
+int booky1;
+int booky2;
+int booky3;
+
+for(int i=0;i<probofteam1.size();i++){
+    for(int j=0;j<probofteam2.size();j++){
+        for(int k=0;k<probofdraw.size();k++){
+
+            if(probofteam1.get(i)+probofteam2.get(j)+probofdraw.get(k)<100.0){
+booky1=i;
+booky2=j;
+booky3=k;
+
+System.out.println("arbitrage opportunity found");
+System.out.println("booky for team1= " + booky1);
+System.out.println("booky for team2= " + booky2);
+System.out.println("booky for draw= " + booky3);
+
+
+            }
+
+        }
+    }
 }
 
 
